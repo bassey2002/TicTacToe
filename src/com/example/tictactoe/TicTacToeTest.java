370d448ef2e7b9a27a34bd5fae2f1240b88f0109
bassey2002 <bassey-godwin.emmanel@binghamuni.edu.ng>
2023-10-24 12:42:21 +0100

    test update

--- src/com/example/tictactoe/TicTacToeTest.java
+++ src/com/example/tictactoe/TicTacToeTest.java
@@ -14,42 +14,11 @@ public class TicTacToeTest {
     }
 
     @Test
-    public void testInitialConditions() {
+    public void testWinningConditions() {
         assertEquals('X', game.getCurrentPlayer());
         assertFalse(game.checkWin());
         assertFalse(game.isBoardFull());
     }
-
-    @Test
-    public void testWinningConditions() {
-        // Test for winning rows
-        game.makeMove(0, 0);
-        game.makeMove(1, 0);
-        game.makeMove(0, 1);
-        game.makeMove(1, 1);
-        game.makeMove(0, 2);
-        assertTrue(game.checkWin());
-
-        // Test for winning columns
-        game.reset();
-        game.makeMove(0, 0);
-        game.makeMove(0, 1);
-        game.makeMove(1, 0);
-        game.makeMove(1, 1);
-        game.makeMove(2, 0);
-        assertTrue(game.checkWin());
-
-        // Test for winning diagonals
-        game.reset();
-        game.makeMove(0, 0);
-        game.makeMove(1, 1);
-        game.makeMove(0, 1);
-        game.makeMove(1, 2);
-        game.makeMove(2, 2);
-        assertTrue(game.checkWin());
-    }
-
-
 
 
     @Test
